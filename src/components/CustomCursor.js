4752bf1eb746ba311ed3b48ec7bd68da71bb0777
M4asl <mateusz.maslowiec98@gmail.com>
2022-11-20 16:34:00 +0100

    Change idea on another project

--- src/components/CustomCursor.js
+++ src/components/CustomCursor.js
@@ -4,15 +4,15 @@ import './CustomCursorStyle.scss';
 const CustomCursor = () => {
   const cursorRef = useRef(null);
 
-  const positionRef = useRef({
-    mouseX: 0,
-    mouseY: 0,
-    destinationX: 0,
-    destinationY: 0,
-    distanceX: 0,
-    distanceY: 0,
-    key: -1,
-  });
+  // const positionRef = useRef({
+  //   mouseX: 0,
+  //   mouseY: 0,
+  //   destinationX: 0,
+  //   destinationY: 0,
+  //   distanceX: 0,
+  //   distanceY: 0,
+  //   key: -1,
+  // });
 
   useEffect(() => {
     document.addEventListener('mousemove', (event) => {
